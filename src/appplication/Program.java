2706e3e36ad828f4b87960b83d188c793b6698a3
lucassafonseca <lucassafonseca@gmail.com>
2022-08-08 19:29:09 -0300

    pinting the board

--- src/appplication/Program.java
+++ src/appplication/Program.java
@@ -1,8 +1,12 @@
 package appplication;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
+		ChessMatch cm = new ChessMatch();
+		UI.printBoard(cm.getPieces());
 	}
 
 }
