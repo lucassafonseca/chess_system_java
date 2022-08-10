8e1b197a72c0515f0b97896ee7dbbdb0e35fa922
lucassafonseca <lucassafonseca@gmail.com>
2022-08-10 16:31:59 -0300

    moving pieces

---
+++ src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Scanner;
+
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ChessMatch cm = new ChessMatch();
+		
+		while(true) {
+			UI.printBoard(cm.getPieces());
+			System.out.println();
+			System.out.print("source :");
+			ChessPosition source = UI.readChessPosition(sc);
+			System.out.println();
+			System.out.println("target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = cm.performChessMove(source, target);
+		}
+		
+	}
+
+}
